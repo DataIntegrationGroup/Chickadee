f290be5d02d0f514e3a97427546d2148d62ea7b4
jross <jake.ross@nmt.edu>
2023-08-10 15:49:53 -0600

    added plotly

--- api/main.py
+++ api/main.py
@@ -13,10 +13,13 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ===============================================================================
+import json
 import os
 from datetime import datetime
 
+import jsbeautifier
 from fastapi import Depends
+from numpy import array, flipud, fliplr
 from sqlalchemy.orm import Session
 from starlette.requests import Request
 from starlette.responses import HTMLResponse
@@ -33,14 +36,13 @@ if int(os.environ.get("CHICKADEE_ERASE_AND_REBUILD_DB", 0)):
     Base.metadata.drop_all(bind=engine)
     Base.metadata.create_all(bind=engine)
 
-
-from routes import sample, project, analysis, material
+from routes import sample, project, analysis, material, process
 
 app.include_router(sample.router)
 app.include_router(project.router)
 app.include_router(analysis.router)
 app.include_router(material.router)
-
+app.include_router(process.router)
 
 BASE_DIR = Path(__file__).resolve().parent
 templates = Jinja2Templates(directory=str(Path(BASE_DIR, "templates")))
@@ -53,10 +55,66 @@ def mapboxtoken():
     }
 
 
+@app.get('/source_sink', response_class=HTMLResponse)
+def source_sink(request: Request, age: str = None, kca: str = None):
+    match = None
+    if age and kca:
+        from routes.process import source_matcher
+        match = source_matcher(age, kca)
+    else:
+        age = 'Age'
+        kca = 'K/Ca'
+
+    if match:
+        import plotly.graph_objects as go
+        fig = go.Figure()
+        xs = match.pop('ages')
+        ys = match.pop('kcas')
+        full_probability = match.pop('full_probability')
+        pxs = match.pop('pxs')
+        pys = match.pop('pys')
+
+        nage, _ = age.split(',')
+        nkca, _ = kca.split(',')
+
+        fig.add_trace(go.Scatter(x=xs, y=ys, mode="markers", name="TestPlot"))
+        fig.add_trace(go.Scatter(x=[nage], y=[nkca], mode="markers",
+                                 marker_size=10,
+                                 marker_symbol='square',
+                                 name="Sink"))
+        fig.add_trace(go.Contour(z=fliplr(flipud(array(full_probability))), x=pxs, y=pys,
+                                 ))
+        fig = fig.to_json()
+
+    return templates.TemplateResponse(
+        "source_sink.html",
+        {
+            "request": request,
+            "age": age,
+            "kca": kca,
+            "match": match,
+            "graphJSON": fig
+        },
+    )
+
+
 @app.get("/", response_class=HTMLResponse)
 def map_view(request: Request):
     return templates.TemplateResponse(
         "map_view.html",
+        {
+            "request": request,
+            # "center": {"lat": 34.5, "lon": -106.0},
+            # "zoom": 7,
+            # "data_url": "/locations/fc",
+        },
+    )
+
+
+@app.get("/tabular", response_class=HTMLResponse)
+def map_view(request: Request):
+    return templates.TemplateResponse(
+        "table_view.html",
         {
             "request": request,
             # "center": {"lat": 34.5, "lon": -106.0},
