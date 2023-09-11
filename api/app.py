74639c3d5a185798f20183b81742745279a99d25
jross <jake.ross@nmt.edu>
2023-09-11 16:39:54 -0600

    root relative

--- api/app.py
+++ api/app.py
@@ -25,6 +25,6 @@ app = FastAPI(
     },
 )
 
-app.mount("/static", StaticFiles(directory="static"), name="static")
+app.mount("/chickadee/static", StaticFiles(directory="static"), name="static")
 
 # ============= EOF =============================================
